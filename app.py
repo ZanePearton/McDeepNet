4f5e2da94e695a4f2bc1bf6216281489656518b4
Zane <z.pearton@accenture.com>
2023-07-20 09:12:08 +1000

    readme

--- app.py
+++ app.py
@@ -44,8 +44,10 @@ def generate_sentence(model, tokenizer, max_length, seed_text, num_words, temper
 # Set up the UI
 st.title(" 🍔 McDeepNet 🍔 ")
 st.subheader("Trained on 20k McDonalds Review's")
-st.text("This project contains a Machine Learning (ML) model that is trained on 20,000 McDonald's reviews. The ML model uses Recurrent Neural Networks (RNNs) to learn patterns from the provided reviews and then, using this learning, generates a unique review of its own. The model can produce different types of output based on seed text and the 'temperature' set by the user.")
+st.text("Welcome to McDeepNet! This project uses a Machine Learning (ML) model trained on 20,000 McDonald's reviews. It's an interesting application that employs Recurrent Neural Networks (RNNs) to learn patterns from these reviews and, subsequently, generates a unique review of its own.")
 
+
+The model can produce varying types of output based on a seed text and a temperature parameter provided by the user.
 # Form to take user inputs
 with st.form(key='my_form'):
     seed_text = st.text_input(label='Enter the seed text for sentence completion')
