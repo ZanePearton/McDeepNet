7f623962978047d1eadccf5a47ec7ad512d2a32d
Zane <z.pearton@accenture.com>
2023-07-20 08:54:15 +1000

    probability print out

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import pandas as pd
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from collections import Counter
-import plotly.express as px
 
 # Load the model
 model = load_model('text_generation_model.h5')
@@ -20,6 +18,7 @@ max_length = 442  # Update this to the maximum length of sequence that you train
 
 # Function to generate a sentence
 def generate_sentence(model, tokenizer, max_length, seed_text, num_words, temperature):
+    word_probs = []
     for _ in range(num_words):
         sequence = tokenizer.texts_to_sequences([seed_text])[0]
         sequence = pad_sequences([sequence], maxlen=max_length-1, padding='pre')
@@ -38,11 +37,12 @@ def generate_sentence(model, tokenizer, max_length, seed_text, num_words, temper
                 output_word = word
                 break
         seed_text += " " + output_word
-    return seed_text
+        word_probs.append((output_word, probabilities[0, predicted]))
+    return seed_text, word_probs
 
 # Set up the UI
 st.title(" 🍔 McDeepNet 🍔 \n Trained on 20k McDonalds Review's ")
-st.text(" This project contains a Machine Learning (ML) model that is trained on 20,000 \n McDonald's reviews. The ML model uses Recurrent Neural Networks (RNNs) to learn \n patterns from the provided reviews and then, using this learning, generates \n a unique review of its own. The model can produce different types of output \n based on seed text and the 'temperature' set by the user.")
+st.text("This project contains a Machine Learning (ML) model that is trained on 20,000 McDonald's reviews. The ML model uses Recurrent Neural Networks (RNNs) to learn patterns from the provided reviews and then, using this learning, generates a unique review of its own. The model can produce different types of output based on seed text and the 'temperature' set by the user.")
 
 # Form to take user inputs
 with st.form(key='my_form'):
@@ -53,6 +53,9 @@ with st.form(key='my_form'):
 
 # Generate and display the output on form submission
 if submit_button:
-    sentence = generate_sentence(model, tokenizer, max_length, seed_text, num_words, temperature)
+    sentence, word_probs = generate_sentence(model, tokenizer, max_length, seed_text, num_words, temperature)
     st.write(sentence)
     
+    # Create dataframe to hold word probabilities and display in Streamlit
+    prob_df = pd.DataFrame(word_probs, columns=["Word", "Probability"])
+    st.dataframe(prob_df)
